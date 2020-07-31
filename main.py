af201a0f439829581af37fe2dd13d4236b4f34b3
khc196 <khc146@gmail.com>
2020-07-31 15:55:50 +0900

    change arguments order

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from splash_code_generator import CodeGenerator
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', '-n', required=True, help='Project name')
-    parser.add_argument('--file', '-f', required=True, help='JSON file')
-    parser.add_argument('--path', '-p', required=True,
+    parser.add_argument('-n', '--name', required=True, help='Project name')
+    parser.add_argument('-f', '--file', required=True, help='JSON file')
+    parser.add_argument('-p', '--path', required=True,
                         help="Your ROS workspace path")
     args = parser.parse_args()
     code_generator = CodeGenerator(args)
