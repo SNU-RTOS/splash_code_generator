f1964bf7f82ece438284386b92008e0ca2a362cd
khc196 <khc146@gmail.com>
2020-08-04 16:28:17 +0900

    Add print output for subprocess

--- splash_code_generator/ros_package_generator/__init__.py
+++ splash_code_generator/ros_package_generator/__init__.py
@@ -4,5 +4,10 @@ import subprocess
 class ROSPackageGenerator:
     def __init__(self, name, path):
         command = "ros2 pkg create --build-type ament_python %s" % (name)
-        subprocess.Popen(command.split(),
-                         cwd=path + "/src", stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(command.split(), cwd=path + "\\src",
+                                   stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out, err = process.communicate()
+        if(out):
+            print(out.decode('utf-8').strip())
+        elif(err):
+            print(err.decode('utf-8').strip())
