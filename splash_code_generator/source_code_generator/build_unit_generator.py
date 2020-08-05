acb03cbd3552cf7f18798b40e8c626a92986af96
khc196 <khc146@gmail.com>
2020-08-05 16:56:50 +0900

    Implement BuildUnitGenerator

--- splash_code_generator/source_code_generator/build_unit_generator.py
+++ splash_code_generator/source_code_generator/build_unit_generator.py
@@ -1,56 +1,100 @@
-
-def append_lines(string, lines, indent):
-    line_list = lines.split('\n')
-    for line in line_list:
-        string = string + (" " * indent * 4) + line + "\n"
-    return string
+from .__util import append_lines
+import copy
 
 
 class BuildUnitGenerator:
-    def __init__(self, node_data):
-        self.__processing_components = node_data["processing_components"]
-        self.__source_components = node_data["source_components"]
-        self.__sink_components = node_data["sink_components"]
-        self.__fusion_operators = node_data["fusion_operators"]
-        self.__factories = node_data["factories"]
-        self.__build_units = node_data["build_units"]
-        self.__stream_ports = node_data["stream_ports"]
-        self.__event_input_ports = node_data["event_input_ports"]
-        self.__event_output_ports = node_data["event_output_ports"]
-        self.__modechange_input_ports = node_data["modechange_input_ports"]
-        self.__modechange_output_ports = node_data["modechange_output_ports"]
-        # self.__links = link_data_list
+    def __init__(self, node_data, link_data):
+        node_data_cp = copy.deepcopy(node_data)
+        self.__processing_components = node_data_cp["processing_components"]
+        self.__source_components = node_data_cp["source_components"]
+        self.__sink_components = node_data_cp["sink_components"]
+        self.__fusion_operators = node_data_cp["fusion_operators"]
+        self.__factories = node_data_cp["factories"]
+        self.__build_units = node_data_cp["build_units"]
+        self.__stream_ports = node_data_cp["stream_ports"]
+        self.__event_input_ports = node_data_cp["event_input_ports"]
+        self.__event_output_ports = node_data_cp["event_output_ports"]
+        self.__modechange_input_ports = node_data_cp["modechange_input_ports"]
+        self.__modechange_output_ports = node_data_cp["modechange_output_ports"]
+        self.__links = link_data
 
     def generate(self):
         build_units = []
+        not_assigned_stream_ports = self.__stream_ports
+        for component in self.__processing_components:
+            component, not_assigned_stream_ports = self.__relate_stream_ports(
+                component, not_assigned_stream_ports)
+        for component in self.__source_components:
+            component, not_assigned_stream_ports = self.__relate_stream_ports(
+                component, not_assigned_stream_ports)
+        for component in self.__sink_components:
+            component, not_assigned_stream_ports = self.__relate_stream_ports(
+                component, not_assigned_stream_ports)
+        for component in self.__fusion_operators:
+            component, not_assigned_stream_ports = self.__relate_stream_ports(
+                component, not_assigned_stream_ports)
         # generate a default build unit
-        default_build_unit = self.__generate_build_unit()
+        default_build_unit = self.__generate_build_unit("")
         if(default_build_unit):
             build_units.append(default_build_unit)
         # generate build units user made
         for _build_unit in self.__build_units:
-            build_units.append(self.__generate_build_unit(_build_unit["key"]))
+            build_units.append(self.__generate_build_unit(
+                _build_unit["key"], _build_unit["name"], _build_unit["class_name"]))
 
         return build_units
 
-    def __generate_build_unit(self, name=""):
+    def __relate_stream_ports(self, component, stream_ports):
+        component["stream_input_ports"] = []
+        component["stream_output_ports"] = []
+        new_stream_port = []
+        for stream_port in stream_ports:
+            if(stream_port["group"] == component["key"]):
+                if(stream_port["PORT_TYPE"] == "STREAM_INPUT_PORT"):
+                    stream_port = self.__simplify_port(stream_port)
+                    stream_port["channel"] = self.__find_channel_name_for_input_port(
+                        stream_port)
+                    component["stream_input_ports"].append(stream_port)
+                elif(stream_port["PORT_TYPE"] == "STREAM_OUTPUT_PORT"):
+                    stream_port = self.__simplify_port(stream_port)
+                    component["stream_output_ports"].append(stream_port)
+            else:
+                new_stream_port.append(stream_port)
+        return component, new_stream_port
+
+    def __find_channel_name_for_input_port(self, input_port):
+        for link in self.__links:
+            if(link["to"] == input_port["key"]):
+                for stream_port in self.__stream_ports:
+                    if(stream_port["PORT_TYPE"] == "STREAM_OUTPUT_PORT" and stream_port["key"] == link["from"]):
+                        return stream_port["Channel"]
+        return None
+
+    def __simplify_port(self, port):
+        try:
+            del port['category']
+            del port['loc']
+            del port['group']
+            del port['buildUnit']
+        except KeyError:
+            pass
+
+        return port
+
+    def __generate_build_unit(self, key, name="", class_name=""):
         build_unit = {}
-        build_unit["name"] = name
-        build_unit["file_name"] = name if name else "default_build_unit"
+        build_unit["name"] = name if name else "default_build_unit"
+        build_unit["class_name"] = class_name if class_name else "DefaultBuildUnit"
         build_unit["processing_components"] = self.__parse_component_info(
-            "processingComponent", name)
+            "processingComponent", key)
         build_unit["source_components"] = self.__parse_component_info(
-            "sourceComponent", name)
+            "sourceComponent", key)
         build_unit["sink_components"] = self.__parse_component_info(
-            "sinkComponent", name)
+            "sinkComponent", key)
         build_unit["fusion_operators"] = self.__parse_component_info(
-            "fusionOperator", name)
+            "fusionOperator", key)
         build_unit["source_code"] = self.__generate_source_code(build_unit)
-        if len(build_unit["processing_components"]) == 0 \
-                and len(build_unit["source_components"]) == 0 \
-                and len(build_unit["sink_components"]) == 0 \
-                and len(build_unit["fusion_operators"]) == 0:
-            return None
+        build_unit["exec_code"] = self.__generate_exec_code(build_unit)
 
         return build_unit
 
@@ -73,39 +117,14 @@ class BuildUnitGenerator:
         for _component in components:
             if(_component["buildUnit"] != build_unit_name):
                 continue
-            component = {}
-            component.update(_component)
-
-            component["stream_input_ports"] = []
-            component["stream_output_ports"] = []
-            component["event_input_ports"] = []
-            component["event_output_ports"] = []
-            component["modechange_input_ports"] = []
-            component["modechange_output_ports"] = []
-            for _stream_port in self.__stream_ports:
-                if(_stream_port["group"] == _component["key"]):
-                    if(_stream_port["PORT_TYPE"] == "STREAM_INPUT_PORT"):
-                        component["stream_input_ports"].append(_stream_port)
-                    elif(_stream_port["PORT_TYPE"] == "STREAM_OUTPUT_PORT"):
-                        component["stream_output_ports"].append(_stream_port)
-
-            for _event_port in self.__event_input_ports:
-                component["event_input_ports"].append(_event_port)
-            for _event_port in self.__event_output_ports:
-                component["event_output_ports"].append(_event_port)
-
-            for _modechange_port in self.__modechange_input_ports:
-                component["modechange_input_ports"].append(_modechange_port)
-            for _modechange_port in self.__modechange_output_ports:
-                component["modechange_output_ports"].append(_modechange_port)
-            result.append(component)
+            result.append(_component)
         return result
 
     def __generate_source_code(self, build_unit):
         _str = ""
         _str = append_lines(_str, "'''", 0)
         _str = append_lines(
-            _str, "Generated automatically by Splash Code Generator for {}".format(build_unit["file_name"]), 1)
+            _str, "Generated automatically by Splash Code Generator for {}".format(build_unit["name"]), 1)
         _str = append_lines(_str, "'''", 0)
         _str = append_lines(_str, self.__import_rcl(), 0)
         _str = append_lines(_str, self.__import_scl_component_node(), 0)
@@ -121,6 +140,20 @@ class BuildUnitGenerator:
         if(len(build_unit["fusion_operators"]) > 0):
             _str = append_lines(_str, self.__import_components(
                 build_unit["fusion_operators"]), 0)
+        _str = append_lines(
+            _str, self.__generate_build_unit_class(build_unit), 0)
+        print(_str)
+        return _str
+
+    def __generate_exec_code(self, build_unit):
+        _str = ""
+        _str = append_lines(_str, "'''", 0)
+        _str = append_lines(
+            _str, "Generated automatically by Splash Code Generator for {}".format(build_unit["name"]), 1)
+        _str = append_lines(_str, "'''", 0)
+        _str = append_lines(_str, self.__import_rcl(), 0)
+        _str = append_lines(
+            _str, self.__import_build_unit(build_unit["name"], build_unit["class_name"]), 0)
         _str = append_lines(_str, self.__generate_main(build_unit), 0)
         print(_str)
         return _str
@@ -129,9 +162,15 @@ class BuildUnitGenerator:
         _str = ""
 
         _str = append_lines(_str, "import rclpy", 0)
-        _str = append_lines(_str, "from rclpy.node import Node", 0)
         _str = append_lines(
             _str, "from rclpy.executor import MultiThreadedExecutor", 0)
+        return _str
+
+    def __import_build_unit(self, name, class_name):
+        _str = ""
+
+        _str = append_lines(
+            _str, "from .splash.{} import {}".format(name, class_name), 0)
         return _str
 
     def __import_scl_component_node(self):
@@ -154,40 +193,60 @@ class BuildUnitGenerator:
         _str = append_lines(_str, "def main(args=None):", 0)
         _str = append_lines(_str, "rclpy.init(args=args)", 1)
         _str = append_lines(_str, "executor = MultiThreadedExecutor()\n", 1)
-        for component in build_unit["processing_components"]:
-            _str = append_lines(
-                _str, self.__generate_lines_for_component(component), 1)
-        for component in build_unit["source_components"]:
-            _str = append_lines(
-                _str, self.__generate_lines_for_component(component), 1)
-        for component in build_unit["sink_components"]:
-            _str = append_lines(
-                _str, self.__generate_lines_for_component(component), 1)
-        for component in build_unit["fusion_operators"]:
-            _str = append_lines(
-                _str, self.__generate_lines_for_component(component), 1)
+        _str = append_lines(_str, "build_unit = {}()".format(
+            build_unit["class_name"]), 1)
+        _str = append_lines(_str, "for node in build_unit.componentNodes:", 1)
+        _str = append_lines(_str, "executor.add_node(node)", 2)
         _str = append_lines(_str, "executor.spin()", 1)
         _str = append_lines(_str, "rclpy.shutdown()", 1)
         return _str
 
+    def __generate_build_unit_class(self, build_unit):
+        _str = ""
+        _str = append_lines(_str, "class {}():".format(
+            build_unit["class_name"]), 0)
+        _str = append_lines(_str, "def __init__(self):", 1)
+        _str = append_lines(_str, "self.componentNodes = []", 2)
+        for component in build_unit["processing_components"]:
+            _str = append_lines(
+                _str, self.__generate_lines_for_component(component), 2)
+        for component in build_unit["source_components"]:
+            _str = append_lines(
+                _str, self.__generate_lines_for_component(component), 2)
+        for component in build_unit["sink_components"]:
+            _str = append_lines(
+                _str, self.__generate_lines_for_component(component), 2)
+        for component in build_unit["fusion_operators"]:
+            _str = append_lines(
+                _str, self.__generate_lines_for_component(component), 2)
+        return _str
+
     def __generate_lines_for_component(self, component):
         _str = ""
+        name = ""
+        factory = ""
+        mode = ""
+        input_ports = []
+        output_ports = []
+        name = component["name"]
+        try:
+            factory = component["group"]
+            mode = component["mode"]
+        except KeyError:
+            pass
+        input_ports = component["stream_input_ports"]
+        output_ports = component["stream_output_ports"]
+
         _str = append_lines(_str, "{} = {}()".format(
             component["name"], component["class_name"]), 0)
         _str = append_lines(_str, "{}.setup()".format(component["name"]), 0)
         _str = append_lines(
+            _str, "{}.set_info(factory=\"{}\", mode=\"{}\", input_ports={}, output_ports={})".format(name, factory, mode, input_ports, output_ports), 0)
+        _str = append_lines(
             _str, "{0}_node = ComponentNode({0})".format(component["name"]), 0)
         _str = append_lines(
-            _str, "executor.add_node({}_node)".format(component["name"]), 0)
+            _str, "self.componentNodes.append({}_node)".format(component["name"]), 0)
         _str = append_lines(_str, "{}.run()".format(component["name"]), 0)
-        return _str
-
-    def __generate_subscription(self, stream_port):
-        _str = ""
-        return _str
-
-    def __generate_publisher(self, stream_port):
-        _str = ""
         return _str
 
     def __generate_service(self, event):
