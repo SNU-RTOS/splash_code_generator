16c52b4c20162913b544597bbb9c07b44287bd83
khc196 <khc146@gmail.com>
2020-08-07 20:50:36 +0900

    Add subprocess for generate package for interfaces

--- splash_code_generator/ros_package_generator/__init__.py
+++ splash_code_generator/ros_package_generator/__init__.py
@@ -3,7 +3,9 @@ import subprocess
 
 class ROSPackageGenerator:
     def __init__(self, name, path):
-        command = "ros2 pkg create --build-type ament_python %s" % (name)
+        command = "ros2 pkg create --build-type ament_python %s --dependencies rclpy %s_interfaces" % (
+            name, name)
+
         process = subprocess.Popen(command.split(), cwd=path + "\\src",
                                    stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         out, err = process.communicate()
@@ -11,3 +13,12 @@ class ROSPackageGenerator:
             print(out.decode('utf-8').strip())
         elif(err):
             print(err.decode('utf-8').strip())
+        command2 = "ros2 pkg create --build-type ament_cmake %s_interfaces" % (
+            name)
+        process2 = subprocess.Popen(command2.split(), cwd=path + "\\src",
+                                    stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        out2, err2 = process2.communicate()
+        if(out2):
+            print(out2.decode('utf-8').strip())
+        elif(err2):
+            print(err2.decode('utf-8').strip())
