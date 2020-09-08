baeba0e8b19c25d33e85c82c9adc4d3dc0ec0591
khc196 <khc146@gmail.com>
2020-09-08 15:56:05 +0900

    Remove interfaces generation

--- splash_code_generator/ros_package_generator/__init__.py
+++ splash_code_generator/ros_package_generator/__init__.py
@@ -13,12 +13,3 @@ class ROSPackageGenerator:
             print(out.decode('utf-8').strip())
         elif(err):
             print(err.decode('utf-8').strip())
-        command2 = "ros2 pkg create --build-type ament_cmake %s_interfaces" % (
-            name)
-        process2 = subprocess.Popen(command2.split(), cwd=path + "/src",
-                                    stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        out2, err2 = process2.communicate()
-        if(out2): 
-            print(out2.decode('utf-8').strip())
-        elif(err2):
-            print(err2.decode('utf-8').strip())
